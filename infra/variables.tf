89d701af8115a074db60db5f031a2946c44e1589
Raparker88 <raparker88@gmail.com>
2026-06-03 14:53:42 -0500

    fingers crossed

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,6 +6,6 @@ variable "aws_region" {
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket to trigger the Lambda function"
-  type        = string
-  default     = "hello-lambda-trigger-bucket-unique-name" # IMPORTANT: Change this to a globally unique name
+  type        =  string
+  default     = "hello-lambda-trigger-bucket-rap" # IMPORTANT: Change this to a globally unique name
 }
